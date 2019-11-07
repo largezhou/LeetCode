47281d38390dfa7612cfdaa2d94467fad62ca521
largezhou <largezhou@hotmail.com>
2019-11-07 17:18:32 +0800

    100 v1

---
+++ 100.相同的树.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * @lc app=leetcode.cn id=100 lang=php
+ *
+ * [100] 相同的树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     public $val = null;
+ *     public $left = null;
+ *     public $right = null;
+ *     function __construct($value) { $this->val = $value; }
+ * }.
+ */
+class Solution
+{
+    /**
+     * @param TreeNode $p
+     * @param TreeNode $q
+     *
+     * @return bool
+     */
+    public function isSameTree($p, $q)
+    {
+        if (!$p && !$q) {
+            return true;
+        } elseif ($p && $q && ($p->val == $q->val)) {
+            return $this->isSameTree($p->left, $q->left) &&
+                $this->isSameTree($p->right, $q->right);
+        } else {
+            return false;
+        }
+    }
+}
+// @lc code=end
