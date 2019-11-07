a7995f3f6e5a3d529d81a568f71e25d987e96f8b
largezhou <largezhou@hotmail.com>
2019-11-07 17:12:32 +0800

    88 v1

---
+++ 88.合并两个有序数组.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * @lc app=leetcode.cn id=88 lang=php
+ *
+ * [88] 合并两个有序数组
+ */
+
+// @lc code=start
+class Solution
+{
+    /**
+     * 与合并两个有序链表相同，略
+     *
+     * @param int[] $nums1
+     * @param int   $m
+     * @param int[] $nums2
+     * @param int   $n
+     *
+     * @return null
+     */
+    public function merge(&$nums1, $m, $nums2, $n)
+    {
+    }
+}
+// @lc code=end
