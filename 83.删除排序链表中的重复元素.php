9ca0e5eb6416df93fc759eb1336f96c061777c45
largezhou <largezhou@hotmail.com>
2019-11-07 17:11:36 +0800

    83 v1

---
+++ 83.删除排序链表中的重复元素.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * @lc app=leetcode.cn id=83 lang=php
+ *
+ * [83] 删除排序链表中的重复元素
+ */
+
+// @lc code=start
+/**
+ * Definition for a singly-linked list.
+ * class ListNode {
+ *     public $val = 0;
+ *     public $next = null;
+ *     function __construct($val) { $this->val = $val; }
+ * }.
+ */
+class Solution
+{
+    /**
+     * 与删除排序数组中，重复元素相同，略.
+     *
+     * @param ListNode $head
+     *
+     * @return ListNode
+     */
+    public function deleteDuplicates($head)
+    {
+    }
+}
+// @lc code=end
