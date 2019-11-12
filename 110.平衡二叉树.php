815fbf470c2c494c13a85b3ffd475e80084c45af
largezhou <largezhou@hotmail.com>
2019-11-12 11:05:48 +0800

    110 v2

--- 110.平衡二叉树.php
+++ 110.平衡二叉树.php
@@ -18,6 +18,8 @@
  */
 class Solution
 {
+    protected $res = true;
+
     /**
      * @param TreeNode $root
      *
@@ -25,22 +27,28 @@ class Solution
      */
     public function isBalanced($root)
     {
-        if (is_null($root)) {
-            return true;
-        } elseif (abs($this->height($root->left, 0) - $this->height($root->right, 0)) <= 1) {
-            return $this->isBalanced($root->left) && $this->isBalanced($root->right);
-        } else {
-            return false;
-        }
+        $this->height($root);
+        return $this->res;
     }
 
-    protected function height($n, $h)
+    protected function height($n)
     {
         if (is_null($n)) {
-            return $h;
+            return 0;
         }
 
-        return max($this->height($n->left, $h + 1), $this->height($n->right, $h + 1));
+        $lh = $this->height($n->left) + 1;
+        $rh = $this->height($n->right) + 1;
+
+        /**
+         * 一直在想，如何在递归求高度时候，自底向上的判断左右高度差，然后终止递归
+         * 虽然这里不能终止递归，但是通过类变量记录结果，每个节点只需遍历一次
+         */
+        if (abs($lh - $rh) > 1) {
+            $this->res = false;
+        }
+
+        return max($lh, $rh);
     }
 }
 // @lc code=end
