220fc5a24698d6314cc0c82fcac9da115ff20ff3
largezhou <largezhou@hotmail.com>
2019-11-15 15:36:37 +0800

    191 v2

--- 191.位-1-的个数.js
+++ 191.位-1-的个数.js
@@ -11,14 +11,10 @@
  */
 var hammingWeight = function (n) {
     var res = 0;
-    for (var i = 0; i < 32; i++) {
-        if (n & 1) {
-            res++;
-        }
-
-        n >>= 1;
+    while (n) {
+        res++;
+        n &= (n - 1);
     }
-
     return res;
 };
 // @lc code=end
